f5218779396d8f7dc69b56136931c43cd9f48206
Ilonka-v-Vliet <157616983+Ilonka-v-Vliet@users.noreply.github.com>
2024-05-29 16:40:34 +0200

    ?????

--- src/__init__.py
+++ src/__init__.py
@@ -1,7 +1,7 @@
 # this is the namespace thing, you need to have the filepath there
 # Because by the nature of psychological experiments, the GUI is a required part of using this
 # this __init__ file may end up only containing a function to launch the GUI
-from src.GUItesting import InitGUI
+#from src.GUItesting import InitGUI
 
 # The only other thing it may contain is a function that takes a config and starts an experiment from there
 
