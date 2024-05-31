a45f73b964680a67bb24d14577c92d5c9b6c9972
Ilonka-v-Vliet <157616983+Ilonka-v-Vliet@users.noreply.github.com>
2024-05-31 15:21:00 +0200

    Update __init__.py
    removed unnecessary comments

--- PsychExperimentsGUI/__init__.py
+++ PsychExperimentsGUI/__init__.py
@@ -1,8 +1,2 @@
-# this is the namespace thing, you need to have the filepath there
-# Because by the nature of psychological experiments, the GUI is a required part of using this
-# this __init__ file may end up only containing a function to launch the GUI
+# This ensures only the function to start the GUI is accessible to the user
 from PsychExperimentsGUI.functions import InitGUI
-
-# The only other thing it may contain is a function that takes a config and starts an experiment from there
-
-#from vanVliet_ptnsPsychExperimentsGUI.GUItesting import ExperimentPress
