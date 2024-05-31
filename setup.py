c8c4a63b2f12a009c935c62cc6d0b50c04929e60
Ilonka-v-Vliet <157616983+Ilonka-v-Vliet@users.noreply.github.com>
2024-05-31 11:14:37 +0200

    dependency problem fixed
    when installing could not find correct version of PIL, it should be installing pillow

--- setup.py
+++ setup.py
@@ -8,7 +8,7 @@ setuptools.setup(
     python_requires = ">=3.6",
     install_requires=[
         "matplotlib",   # version 3.8.4 used when making this
-        "PIL",          # version pillow 10.3.0 used when making this
+        "pillow",          # version pillow 10.3.0 used when making this
         "pandas",       # version 2.2.2 used when making this
     ],
     packages = setuptools.find_packages()
